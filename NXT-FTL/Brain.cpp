b6e81f6883d41e0e56e23a534e3ef4858a6d997d
jeepee12 <jeepee.12@hotmail.com>
2018-02-07 09:53:32 -0500

    Updating Brain Interface.

--- NXT-FTL/Brain.cpp
+++ NXT-FTL/Brain.cpp
@@ -2,11 +2,16 @@
 
 
 
-Brain::Brain()
+Brain::Brain(int stopDistance)
 {
 }
 
 
 Brain::~Brain()
 {
+}
+
+std::tuple<int, bool> Brain::ComputeDirection(FTLTouchSensor touchSensor, FTLDistanceSensor distanceSensor, FTLColorSensor leftColorSensor, FTLColorSensor rightColorSensor)
+{
+	// The distance
 }
--- NXT-FTL/Brain.h
+++ NXT-FTL/Brain.h
@@ -2,12 +2,16 @@
 #include "FTLTouchSensor.h"
 #include "FTLDistanceSensor.h"
 #include "FTLColorSensor.h"
+#include <tuple>
 
 class Brain
 {
+	int stopDistance;
 public:
-	Brain();
+	Brain(int stopDistance);
 	~Brain();
-	int ComputeDirection(FTLTouchSensor, FTLDistanceSensor, FTLColorSensor, FTLColorSensor);
+
+	//Compute de direction that the robot need to take to follow the line. Negative represent left and positive reprensent right.
+	std::tuple<int, bool> ComputeDirection(FTLTouchSensor touchSensor, FTLDistanceSensor distanceSensor, FTLColorSensor leftColorSensor, FTLColorSensor rightColorSensor);
 };
 
