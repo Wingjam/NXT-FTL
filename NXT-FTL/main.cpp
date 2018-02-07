caed5ed435d4bb0d7fed128406a97031d0ddbb71
Jonathan Martineau <jo_martineau_7@hotmail.com>
2018-02-07 12:00:07 -0500

    Added initial logic in main.cpp

--- NXT-FTL/main.cpp
+++ NXT-FTL/main.cpp
@@ -1,40 +1,52 @@
-#include <cstdlib>  
-#include <iostream>
-#include <string>
-#include <conio.h>
-#include "../NxtLibrary/nxt.h"
+#include "Brain.h"
+#include "communication.h"
 
 using namespace std;
 
-//set up the NXT  
-Connection *connection = new Bluetooth();
-Sensor *sensor1 = new Touch(IN_1, connection);
-Motor *motorB = new Motor(OUT_B, connection);
 int main()
 {
-    try {
-        cout << "Try to connect to the NXT" << endl;
-        connection->connect(40);
-        cout << "Connected" << endl;
-        //connection->connect(1500, "192.168.0.1"); //Network connection  
-        cout << "Press touch sensor - hit any key to end" << endl;
-        while (!_kbhit()) {//hit a key to end  
-            if (sensor1->read()) {
-                motorB->on(75);
-            }
-            else {
-                motorB->stop();
-            }
+    // Init
+    communication communication{};
+    Brain brain { 10 };
+
+    communication.connect(communication::BLUETOOF, 40);
+
+    auto leftMotor = communication.initializeMotor(communication::OUT_A);
+    auto rightMotor = communication.initializeMotor(communication::OUT_C);
+
+    auto touchSensor = TouchSensorDto{};
+    auto leftColorSensor = ColorSensorDto{};
+    auto rightColorSensor = ColorSensorDto{};
+    auto distanceSensor = DistanceSensorDto{};
+
+    communication.initializeSensor(touchSensor, communication::IN_1);
+    communication.initializeSensor(leftColorSensor, communication::IN_2);
+    communication.initializeSensor(rightColorSensor, communication::IN_3);
+    communication.initializeSensor(distanceSensor, communication::IN_4);
+
+
+    while (true)
+    {
+        // Read
+        communication.updateSensorValue(touchSensor);
+        communication.updateSensorValue(leftColorSensor);
+        communication.updateSensorValue(rightColorSensor);
+        communication.updateSensorValue(distanceSensor);
+
+        // Process
+        tuple<int, bool> direction = brain.ComputeDirection(touchSensor, distanceSensor, leftColorSensor, rightColorSensor);
+
+        // Send
+
+        if (!get<1>(direction))
+        {
+            communication.stopMotor(leftMotor);
+            communication.stopMotor(rightMotor);
+            break;
         }
-        connection->disconnect();
+
+        communication.startMotor(leftMotor, 75);
+        communication.startMotor(rightMotor, 75);
     }
-    catch (Nxt_exception& e) {
-        //some error occurred - print it out  
-        cout << e.what() << endl;
-        cout << "error code: " << e.error_code() << endl;
-        cout << "error type: " << e.error_type() << endl;
-        cout << e.who() << endl;
-        connection->disconnect();
-    }
-    return 0;
+
 }
