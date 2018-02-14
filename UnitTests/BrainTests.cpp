59c18f687095fad41ac50ffc7030262c5af5dcc7
fvigneault <minivigno@gmail.com>
2018-02-14 10:09:56 -0500

    Added small documentation on tests operators

--- UnitTests/BrainTests.cpp
+++ UnitTests/BrainTests.cpp
@@ -2,6 +2,43 @@
 #include "../NXT-FTL/Brain.h"
 #include "../NXT-FTL/Brain.cpp"
 
+/*
+
+ASSERT_* yields a fatal failure and returns from the current function
+EXPECT_* yields a nonfatal failure, allowing the function to continue running
+
+Basic operators :
+ASSERT_TRUE(condition)
+ASSERT_FALSE(condition)
+ASSERT_EQ(val1, val2)
+ASSERT_NE(val1, val2)
+ASSERT_LT(val1, val2)
+ASSERT_LE(val1, val2)
+ASSERT_GT(val1, val2)
+ASSERT_GE(val1, val2)
+
+EXPECT_TRUE(condition)
+EXPECT_FALSE(condition)
+EXPECT_EQ(val1, val2)
+EXPECT_NE(val1, val2)
+EXPECT_LT(val1, val2)
+EXPECT_LE(val1, val2)
+EXPECT_GT(val1, val2)
+EXPECT_GE(val1, val2)
+
+String operators : 
+ASSERT_STREQ(str1, str2)
+ASSERT_STRNE(str1, str2)
+ASSERT_STRCASEEQ(str1, str2) : ignores case
+ASSERT_STRCASENE(str1, str2) : ignores case
+
+EXPECT_STREQ(str1, str2)
+EXPECT_STRNE(str1, str2)
+EXPECT_STRCASEEQ(str1, str2) : ignores case
+EXPECT_STRCASENE(str1, str2) : ignores case
+
+*/
+
 static unsigned int STOP_DISTANCE = 10;
 
 TEST(BrainTest, BrainInitialization) {
