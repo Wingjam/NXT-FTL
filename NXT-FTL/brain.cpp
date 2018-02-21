58e2f28322d5017db9dc25c70909a147d58162cb
jeepee12 <jeepee.12@hotmail.com>
2018-02-21 11:16:36 -0500

    Change the names for Miss Jonathan requests.

--- NXT-FTL/brain.cpp
+++ NXT-FTL/brain.cpp
@@ -8,18 +8,17 @@ brain::brain(unsigned int stopDistance) : stopDistance(stopDistance)
 
 std::tuple<int, bool> brain::compute_direction(const touch_sensor_dto touch_sensor, const distance_sensor_dto distance_sensor, color_sensor_dto left_color_sensor, color_sensor_dto right_color_sensor) const
 {
-	bool stopping = false;
+	bool needToStop = false;
 	// The touch sensor stop the robot
-	//if touchsenso is touch => stop
-	stopping |= touch_sensor.is_pressed;
+	// If touchsenso is touch => stop
+	needToStop |= touch_sensor.is_pressed;
 
 	// The distance before the robot stop
-	// if distanceSensor.distance < stopDistance => stop
-	stopping |= distance_sensor.distance <= stopDistance;
+	// If distanceSensor.distance < stopDistance => stop
+	needToStop |= distance_sensor.distance <= stopDistance;
 
-	int direction{ 0 };
 	// Calculate the direction of the robot
-	direction = left_color_sensor.value() - right_color_sensor.value();
+	int direction{ left_color_sensor.value() - right_color_sensor.value() };
 
-	return std::tuple<int, bool>{direction, !stopping};
+	return std::tuple<int, bool>{direction, !needToStop};
 }
