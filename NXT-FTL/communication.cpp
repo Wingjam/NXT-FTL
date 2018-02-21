b67cc5400032748c859e0e4f9390d1010a133176
Raphaël Cayouette <raphael.cayouette@usherbrooke.ca>
2018-02-21 12:00:09 -0500

    Add documentation to communication library

--- NXT-FTL/communication.cpp
+++ NXT-FTL/communication.cpp
@@ -3,15 +3,32 @@
 
 using namespace nxtftl;
 
+/// <summary>
+/// 
+/// </summary>
+/// <param name=""></param>
+/// <returns></returns>
 communication::communication()
 {
 }
 
-
+/// <summary>
+/// 
+/// </summary>
+/// <param name=""></param>
+/// <returns></returns>
 communication::~communication()
 {
 }
 
+/// <summary>
+/// Initialize connection to NXT brick
+/// If bluetooth mode is selected, this method will try to find 
+/// the comport connected to the brick.
+/// USB connection mode is not yet implemented.
+/// </summary>
+/// <param name="type">Type of connection to the brick (USB/Bluetooth)</param>
+/// <returns>Connection success result</returns>
 bool communication::connect(ConnectionType type)
 {
 	bool connected = false;
@@ -42,6 +59,11 @@ bool communication::connect(ConnectionType type)
 	}
 }
 
+/// <summary>
+/// This method connects to the brick using bluetooth
+/// </summary>
+/// <param name="comport">Bluetooth comport for the NXT</param>
+/// <returns>Connection success result</returns>
 bool communication::connectWithBluetooth(unsigned int comport) 
 {
 	try
@@ -57,6 +79,10 @@ bool communication::connectWithBluetooth(unsigned int comport)
 	}
 }
 
+/// <summary>
+/// Stops all motors and disconnects from the NXT cleanly
+/// </summary>
+/// <returns>Operation success result</returns>
 bool communication::disconnect()
 {
 	bool success = false;
@@ -72,6 +98,10 @@ bool communication::disconnect()
 	return success;
 }
 
+/// <summary>
+/// Prints an NXT Exception error
+/// </summary>
+/// <param name="e">Nxt_exception error</param>
 void communication::printError(Nxt_exception& e)
 {
 	cout << e.what() << endl;
@@ -80,6 +110,11 @@ void communication::printError(Nxt_exception& e)
 	cout << e.who() << endl;
 }
 
+/// <summary>
+/// Initializes a touch sensor
+/// </summary>
+/// <param name="touchSensorDto">Reference to the touch sensor to initialize</param>
+/// <param name="port">Port of the sensor on the brick</param>
 void communication::initializeSensor(touch_sensor_dto & touchSensorDto, SensorPort port)
 {
 	Touch* touchSensor = new Touch(mapSensorPort(port), connection);
@@ -87,6 +122,11 @@ void communication::initializeSensor(touch_sensor_dto & touchSensorDto, SensorPo
 	touchSensors.emplace(port, touchSensor);
 }
 
+/// <summary>
+/// Initializes a color sensor
+/// </summary>
+/// <param name="colorSensorDto">Reference to the color sensor to initialize</param>
+/// <param name="port">Port of the sensor on the brick</param>
 void communication::initializeSensor(color_sensor_dto& colorSensorDto, SensorPort port)
 {
 	color2* colorSensor = new color2(mapSensorPort(port), connection);
@@ -94,6 +134,11 @@ void communication::initializeSensor(color_sensor_dto& colorSensorDto, SensorPor
 	colorSensors.emplace(port, colorSensor);
 }
 
+/// <summary>
+/// Initializes a distance sensor
+/// </summary>
+/// <param name="distanceSensorDto">Reference to the distance sensor to initialize</param>
+/// <param name="port">Port of the sensor on the brick</param>
 void communication::initializeSensor(distance_sensor_dto& distanceSensorDto, SensorPort port)
 {
 	Sonar* distanceSensor = new Sonar(mapSensorPort(port), connection);
@@ -101,6 +146,10 @@ void communication::initializeSensor(distance_sensor_dto& distanceSensorDto, Sen
 	distanceSensors.emplace(port, distanceSensor);
 }
 
+/// <summary>
+/// Initializes a motor
+/// </summary>
+/// <param name="port">Port of the motor on the brick</param>
 motor_dto communication::initializeMotor(MotorPort port)
 {
 	Motor* motor = new Motor(mapMotorPort(port), connection);
@@ -108,6 +157,10 @@ motor_dto communication::initializeMotor(MotorPort port)
 	return motor_dto{ static_cast<unsigned int>(port) };
 }
 
+/// <summary>
+/// Maps between the sensor dtos and the library objects
+/// </summary>
+/// <param name="port">Port of the sensor on the brick</param>
 Sensor_port communication::mapSensorPort(SensorPort port) 
 {
 	Sensor_port mappedPort;
@@ -129,6 +182,10 @@ Sensor_port communication::mapSensorPort(SensorPort port)
 	return mappedPort;
 }
 
+/// <summary>
+/// Maps between the motor dtos and the library objects
+/// </summary>
+/// <param name="port">Port of the motor on the brick</param>
 Motor_port communication::mapMotorPort(MotorPort port)
 {
 	Motor_port mappedPort;
@@ -147,18 +204,30 @@ Motor_port communication::mapMotorPort(MotorPort port)
 	return mappedPort;
 }
 
+/// <summary>
+/// Reads and updates the touch sensor value
+/// </summary>
+/// <param name="touchSensorDto">Sensor dto</param>
 void communication::updateSensorValue(touch_sensor_dto& touchSensorDto)
 {
 	Touch* touchSensor = touchSensors[touchSensorDto.port];
 	touchSensorDto.is_pressed = touchSensor->read();
 }
 
+/// <summary>
+/// Reads and updates the color sensor value
+/// </summary>
+/// <param name="colorSensorDto">Sensor dto</param>
 void communication::updateSensorValue(color_sensor_dto& colorSensorDto)
 {
 	color2* colorSensor = colorSensors[colorSensorDto.port];
 	colorSensorDto.intensity = colorSensor->getValue();
 }
 
+/// <summary>
+/// Reads and updates the distance sensor value
+/// </summary>
+/// <param name="distanceSensorDto">Sensor dto</param>
 void communication::updateSensorValue(distance_sensor_dto& distanceSensorDto)
 {
 	Sonar* distanceSensor = distanceSensors[distanceSensorDto.port];
@@ -166,32 +235,59 @@ void communication::updateSensorValue(distance_sensor_dto& distanceSensorDto)
 	distanceSensorDto.distance = value;
 }
 
+/// <summary>
+/// Stops all initialized motors connected to the brick
+/// </summary>
 void communication::stopAllMotors() {
 	for_each(motors.begin(), motors.end(), [](pair<int, Motor*> pair) { pair.second->stop(); });
 }
 
+/// <summary>
+/// Resets all color sensors connected to the brick
+/// </summary>
 void communication::resetSensors() {
 	//for_each(colorSensors.begin(), colorSensors.end(), [](pair<int, Sensor*> pair) { pair.second->reset(); });
 }
 
+/// <summary>
+/// Determines if a motor is currently running
+/// </summary>
+/// <param name="motorDto">Motor to verify</param>
 bool communication::isMotorRunning(motor_dto motorDto)
 {
 	Motor* motor = motors[motorDto.port];
 	return motor->is_running();
 }
 
+/// <summary>
+/// Starts a motor (on)
+/// </summary>
+/// <param name="motorDto">Motor to start</param>
+/// <param name="speed">Speed of the motor (0-100)</param>
+/// <param name="degrees">Degrees of rotation (0-360)</param>
+/// <param name="reply">Force NXT to reply (false by default)</param>
 void communication::startMotor(motor_dto motorDto, char speed, unsigned int degrees /* = 0 */, bool reply /* = false */)
 {
 	Motor* motor = motors[motorDto.port];
 	motor->on(speed, degrees, reply);
 }
 
+/// <summary>
+/// Coasts a motor (off) (motor will not brake)
+/// </summary>
+/// <param name="motorDto">Motor to coast</param>
+/// <param name="reply">Force NXT to reply (false by default)</param>
 void communication::coastMotor(motor_dto motorDto, bool reply /* = false */)
 {
 	Motor* motor = motors[motorDto.port];
 	motor->off(reply);
 }
 
+/// <summary>
+/// Stops a motor (brake)
+/// </summary>
+/// <param name="motorDto">Motor to stop</param>
+/// <param name="reply">Force NXT to reply (false by default)</param>
 void communication::stopMotor(motor_dto motorDto, bool reply /* = false */)
 {
 	Motor* motor = motors[motorDto.port];
--- NXT-FTL/main.cpp
+++ NXT-FTL/main.cpp
@@ -65,6 +65,5 @@ int main()
 		}
     }
 
-	communication.stopAllMotors();
 	communication.disconnect();
 }
