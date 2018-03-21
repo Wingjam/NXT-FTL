086418d8540860c20bdd4a11a585ba9f8bdd6949
Felix Vigneault <minivigno@gmail.com>
2018-03-21 12:14:43 -0400

    Fixed merge

--- NXT-FTL/main.cpp
+++ NXT-FTL/main.cpp
@@ -8,13 +8,4 @@ int main()
 {
 	follower follow{ 10 };
 	follow.Run();
-
-	ofstream myfile;
-	myfile.open("output.txt");
-	std::vector<movement_history::position> positions = movement_history.get_positions();
-	for each (movement_history::position position in positions)
-	{
-		myfile << "(" << position.x << "," << position.y << ")";
-	}
-	myfile.close();
 }
