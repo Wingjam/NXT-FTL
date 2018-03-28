b0875dc73c24e48cda0628b5ec1404a1db3133ef
jeepee12 <jeepee.12@hotmail.com>
2018-03-28 11:43:24 -0400

    Reactive the time limit to calculate the direction.

--- NXT-FTL/follower.cpp
+++ NXT-FTL/follower.cpp
@@ -63,25 +63,25 @@ void follower::Run()
 
 		tuple<int, bool, bool> direction;
 		std::chrono::system_clock::time_point max_wait_time = std::chrono::system_clock::now() + std::chrono::microseconds(max_process_time);
-		//std::future<void> updates = std::async(std::launch::async, [&]
-		//{
+		std::future<void> updates = std::async(std::launch::async, [&]
+		{
 			// Process
 			direction = brain.compute_direction(touchSensor, distanceSensor, leftColorSensor, rightColorSensor);
 
 			long int left_motor_tacho_count = communication.get_tacho_count(leftMotor);
 			long int right_motor_tacho_count = communication.get_tacho_count(rightMotor);
 			movement_history.log_rotation(left_motor_tacho_count, right_motor_tacho_count);
-		//});
+		});
 
-		//bool succeeded = std::future_status::ready == updates.wait_until(max_wait_time);
+		bool succeeded = std::future_status::ready == updates.wait_until(max_wait_time);
 		//auto end = std::chrono::system_clock::now();
 		//std::cout << "Remaning time until timeout:" <<
 		//	std::chrono::duration_cast<std::chrono::microseconds>(max_wait_time - end).count() <<
 		//	"us." << endl;
-		//if (!succeeded)
-		//{
-		//	direction = tuple<int, bool, bool>{ 0, true, false };
-		//}
+		if (!succeeded)
+		{
+			direction = tuple<int, bool, bool>{ 0, true, false };
+		}
 
 		// Send
 		bool needsToStop = get<2>(direction);
@@ -96,11 +96,11 @@ void follower::Run()
 		{
 			communication.stopMotor(leftMotor);
 			communication.stopMotor(rightMotor);
-			//if (!succeeded)
-			//{
-			//	// Now that the robot is stop, we can wait for the robot to give us an answer.
-			//	updates.wait();
-			//}
+			if (!succeeded)
+			{
+				// Now that the robot is stop, we can wait for the robot to give us an answer.
+				updates.wait();
+			}
 			continue;
 		}
 
