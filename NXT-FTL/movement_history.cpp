75c7fcf9f6f3fd8de1696de987decb288d426b20
Felix Vigneault <minivigno@gmail.com>
2018-03-28 13:16:42 -0400

    Auto stash before merge of "master" and "origin/master"

--- NXT-FTL/movement_history.cpp
+++ NXT-FTL/movement_history.cpp
@@ -24,24 +24,13 @@ position movement_history::calculate_new_position(position initial_position, sna
 	float left_tacho_count_from_last_snapshot = destination_snapshot.left_motor_tacho_count - initial_snapshot.left_motor_tacho_count;
 	float right_tacho_count_from_last_snapshot = destination_snapshot.right_motor_tacho_count - initial_snapshot.right_motor_tacho_count;
 
-	float full_rotation = 100;
+	float full_rotation = 360;
 	float sl_distance_left_wheel = left_tacho_count_from_last_snapshot / full_rotation * WHEEL_CIRCUMFERENCE;
 	float sr_distance_right_wheel = right_tacho_count_from_last_snapshot / full_rotation * WHEEL_CIRCUMFERENCE;
 
-	float O_angle_of_turn_in_rad = std::abs(sl_distance_left_wheel - sr_distance_right_wheel) / DISTANCE_BETWEEN_WHEELS;
-	float r_turn_radius_left_wheel = std::fmin(sr_distance_right_wheel, sl_distance_left_wheel) / O_angle_of_turn_in_rad;
-
-	float initial_axle_angle_rad = initial_position.direction_in_rad - (PI / 2);
-
-	float direction_sign_multiplicator = (sl_distance_left_wheel > sr_distance_right_wheel ? 1 : -1);
-	float distance_from_center_of_rotation_to_center_of_robot = r_turn_radius_left_wheel + (DISTANCE_BETWEEN_WHEELS / 2);
-	float center_of_rotation_x = initial_position.x + direction_sign_multiplicator * cos(initial_axle_angle_rad) * distance_from_center_of_rotation_to_center_of_robot;
-	float center_of_rotation_y = initial_position.y + direction_sign_multiplicator * sin(initial_axle_angle_rad) * distance_from_center_of_rotation_to_center_of_robot;
-
-	float final_axle_angle_rad = initial_axle_angle_rad + (-direction_sign_multiplicator * O_angle_of_turn_in_rad);
-
 	position new_position{};
-	if (AreSame(O_angle_of_turn_in_rad, 0.0f))
+
+	if (AreSame(sl_distance_left_wheel, sr_distance_right_wheel))
 	{
 		new_position.direction_in_rad = initial_position.direction_in_rad;
 		new_position.x = initial_position.x + cos(initial_position.direction_in_rad) * sl_distance_left_wheel;
@@ -49,11 +38,12 @@ position movement_history::calculate_new_position(position initial_position, sna
 	}
 	else
 	{
-		new_position.direction_in_rad = final_axle_angle_rad + (PI / 2);
-		new_position.x = center_of_rotation_x + sin(final_axle_angle_rad) * distance_from_center_of_rotation_to_center_of_robot;
-		new_position.y = center_of_rotation_y + cos(final_axle_angle_rad) * distance_from_center_of_rotation_to_center_of_robot;
+		new_position.direction_in_rad = (sr_distance_right_wheel - sl_distance_left_wheel) / DISTANCE_BETWEEN_WHEELS + initial_position.direction_in_rad;
+		new_position.x = initial_position.x + (DISTANCE_BETWEEN_WHEELS * (sr_distance_right_wheel + sl_distance_left_wheel)) / (2 * (sr_distance_right_wheel - sl_distance_left_wheel))
+			* (sin((sr_distance_right_wheel - sl_distance_left_wheel) / DISTANCE_BETWEEN_WHEELS + initial_position.direction_in_rad) - sin(initial_position.direction_in_rad));
+		new_position.y = initial_position.y - (DISTANCE_BETWEEN_WHEELS * (sr_distance_right_wheel + sl_distance_left_wheel)) / (2 * (sr_distance_right_wheel - sl_distance_left_wheel))
+			* (cos((sr_distance_right_wheel - sl_distance_left_wheel) / DISTANCE_BETWEEN_WHEELS + initial_position.direction_in_rad) - cos(initial_position.direction_in_rad));
 	}
-
 	return new_position;
 }
 
