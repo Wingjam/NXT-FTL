fcf43f6abc6c32305dbaaca8723f22d4720f9234
Felix Vigneault <minivigno@gmail.com>
2018-04-02 15:04:57 -0400

    Fixed some bugs

--- NXT-FTL/buffer_manager.cpp
+++ NXT-FTL/buffer_manager.cpp
@@ -1,3 +1,102 @@
 #include "buffer_manager.h"
 
+template<class data>
+bool buffer_manager<data>::increment_write_buffer() {
+    int incremented_write_buffer_index = utils::increment_value(current_write_buffer_index.load(), number_of_buffers - 1);
 
+    if (incremented_write_buffer_index == current_read_buffer_index.load()) {
+        // Buffers are full, should wait but not now because only the next push cannot be added.
+        return false; // did not increment
+    }
+
+    current_write_buffer_index.store(incremented_write_buffer_index);
+    return true; // did increment
+}
+
+template<class data>
+vector<data>* buffer_manager<data>::get_current_write_buffer() {
+    return &(buffers[current_write_buffer_index.load()]);
+}
+
+template<class data>
+bool buffer_manager<data>::is_read_buffer_empty() {
+    return current_read_buffer_index.load() == current_write_buffer_index.load();
+}
+
+template<class data>
+bool buffer_manager<data>::is_current_write_buffer_full() {
+    return get_current_write_buffer()->size() == buffer_capacity;
+}
+
+template<class data>
+buffer_manager<data>::buffer_manager(int number_of_buffers, int buffer_capacity) {
+    this->number_of_buffers = number_of_buffers;
+    this->buffer_capacity = buffer_capacity;
+
+    for (int i = 0; i < number_of_buffers; ++i) {
+        vector<data> current_buffer{};
+        current_buffer.reserve(buffer_capacity);
+        buffers.push_back(current_buffer);
+    }
+}
+
+template<class data>
+vector<data>* buffer_manager<data>::get_current_read_buffer() {
+    bool is_adding_completed = adding_completed.load();
+    if ((!is_adding_completed && is_read_buffer_empty())
+        ||
+        (is_adding_completed && current_read_buffer_index.load() == utils::increment_value(current_write_buffer_index.load(), number_of_buffers - 1))) {
+        return nullptr; // No read buffer available
+    }
+    return &(buffers[current_read_buffer_index.load()]);
+}
+
+template<class data>
+void buffer_manager<data>::signal_current_buffer_completly_read()
+{
+    vector<data>* current_read_buffer = get_current_read_buffer();
+    if (current_read_buffer != nullptr) {
+        // reset old data vector
+        current_read_buffer->clear();
+        current_read_buffer->reserve(buffer_capacity);
+
+        int next_current_read_buffer_index = utils::increment_value(current_read_buffer_index.load(), number_of_buffers - 1);
+        current_read_buffer_index.store(next_current_read_buffer_index);
+    }
+}
+
+template<class data>
+bool buffer_manager<data>::push_back(data data_to_push)
+{
+    if (adding_completed.load()) {
+        return false;
+    }
+
+    if (is_current_write_buffer_full()) {
+        // Buffer was full last time we pushed, try to increment it again
+        if (!increment_write_buffer()) {
+            // buffer is full
+            return false; // did not add, signal it
+        }
+    }
+
+    vector<data>* current_write_buffer = get_current_write_buffer();
+
+    current_write_buffer->push_back(data_to_push);
+
+    if (current_write_buffer->size() == buffer_capacity) {
+        increment_write_buffer();
+    }
+
+    return true;
+}
+
+template<class data>
+void buffer_manager<data>::complete_adding()
+{
+    adding_completed.store(true);
+}
+
+
+#include "position.h"
+template class buffer_manager<position>;
--- NXT-FTL/buffer_manager.h
+++ NXT-FTL/buffer_manager.h
@@ -9,90 +9,25 @@ using namespace std;
 template<class data>
 class buffer_manager
 {
-	int number_of_buffers = 0;
-	int buffer_capacity = 0;
+    int number_of_buffers = 0;
+    int buffer_capacity = 0;
 
-	vector<vector<data>> buffers;
+    vector<vector<data>> buffers;
 
-	atomic<int> current_write_buffer_index = 0;
-	atomic<int> current_read_buffer_index = 0;
+    atomic<bool> adding_completed{ false };
+    atomic<int> current_write_buffer_index = 0;
+    atomic<int> current_read_buffer_index = 0;
 
-	bool increment_write_buffer() {
-		int incremented_write_buffer_index = utils::increment_value(current_write_buffer_index.load(), number_of_buffers);
-
-		if (incremented_write_buffer_index == current_read_buffer_index.load()) {
-			// Buffers are full, should wait but not now because only the next push cannot be added.
-			return false; // did not increment
-		}
-
-		current_write_buffer_index.store(incremented_write_buffer_index);
-		return true; // did increment
-	}
-
-	vector<data> get_current_write_buffer() {
-		return buffers[current_write_buffer_index.load()];
-	}
-
-	bool is_read_buffer_empty() {
-		return current_read_buffer_index.load() == current_write_buffer_index.load();
-	}
-
-	bool is_current_write_buffer_full() {
-		return get_current_write_buffer().size() == buffer_capacity;
-	}
+    bool increment_write_buffer();
+    vector<data>* get_current_write_buffer();
+    bool is_read_buffer_empty();
+    bool is_current_write_buffer_full();
 
 public:
-	buffer_manager() = default;
-	buffer_manager(int number_of_buffers, int buffer_capacity) {
-		this->number_of_buffers = number_of_buffers;
-		this->buffer_capacity = buffer_capacity;
+    buffer_manager(int number_of_buffers, int buffer_capacity);
 
-		for (int i = 0; i < number_of_buffers; ++i) {
-			vector<data> current_buffer{};
-			current_buffer.reserve(buffer_capacity);
-			buffers.push_back(current_buffer);
-		}
-	}
-	~buffer_manager() = default;
-
-	vector<data> get_current_read_buffer() {
-		if (is_read_buffer_empty()) {
-			return null; // No read buffer available
-		}
-		return buffers[current_read_buffer_index.load()];
-	}
-
-	void signal_current_buffer_completly_read()
-	{
-		if (!is_read_buffer_empty()) {
-			// reset old data vector
-			get_current_read_buffer().clear();
-			get_current_read_buffer().reserve(buffer_capacity);
-
-			int next_current_read_buffer_index = utils::increment_value(current_read_buffer_index.load(), number_of_buffers);
-			current_read_buffer_index.store(next_current_read_buffer_index);
-		}
-	}
-
-	bool push_back(data data)
-	{
-		if (is_current_write_buffer_full()) {
-			// Buffer was full last time we pushed, try to increment it again
-			if (!increment_write_buffer()) {
-				// buffer is full
-				return false; // did not add, signal it
-			}
-		}
-
-		vector<data> current_write_buffer = get_current_write_buffer();
-
-		current_write_buffer.push_back(data);
-		
-		if (current_write_buffer.size() == buffer_capacity) {
-			increment_write_buffer();
-		}
-
-		return true;
-	}
+    vector<data>* get_current_read_buffer();
+    void signal_current_buffer_completly_read();
+    bool push_back(data data_to_push);
+    void complete_adding();
 };
-
